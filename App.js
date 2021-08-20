552eec2ab0752d1e061815aac65bfa00728a7684
Todd Bauermeister <todd@octoco.ltd>
2021-08-20 17:35:42 +0200

    setup base structure for new app

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+      <View>
+
+      </View>
+      <View>
+
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
