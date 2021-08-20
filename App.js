7d2a5b462fe1e219f72019ba8216acc0578edebc
Todd Bauermeister <todd@octoco.ltd>
2021-08-20 18:24:34 +0200

    Add styles to make input and button sit next to each other and look nice

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { Button, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 30}}> 
-      <View style={{flexDirection: 'row'}}>
-        <TextInput placeholder="Course goal" style={{borderColor: 'black', borderWidth: 1, padding: 10}}/>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course goal" style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
         <Button title="ADD"/>
       </View>
       <View>
